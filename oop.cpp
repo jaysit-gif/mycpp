422dc2f3f73207b8b62d71afce0130d280ee4cc5
D J <jayadithya.dhurwey@gmail.com>
2025-07-28 17:09:29 +0530

    new vector improvements

--- oop.cpp
+++ oop.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <ostream>
-typedef struct vector{
-    int i;
-    int j;
-    int k;
-}vector;
+struct vector{
+    int i,j,k; 
+    vector(int i_init = 0,int j_init = 0,int k_init = 0):i(i_init),j(j_init),k(k_init){}//**what happens if i fill "{}" */
+};
 
-vector operator+(const vector &a,const vector &b){ /**why are we using vector &a why not &vector a */
+vector operator+(const vector &a,const vector &b){ 
     vector c;
-    c.i = a.i+b.i;c.j=a.j+b.j;c.k = a.k+ b.k;  /**what is const??*/
+    c.i = a.i+b.i;c.j=a.j+b.j;c.k = a.k+ b.k;  
     return c;
 }
 
-std::ostream& operator<<(std::ostream& os, const vector& v) { /**what is ostream, what does ostream& os mean*/
+std::ostream& operator<<(std::ostream& os, const vector& v) { 
     os << "(" << v.i << ", " << v.j << ", " << v.k << ")";
     return os;
 }
 
+int operator*(const vector &a,const vector &b){ /**dot product */
+    int c;
+    c = a.i*b.i + a.j*b.j + a.k*b.k;
+    return c;
+}
+
 int main(void){
-    vector a,b;
-    a.i = 6;
-    a.j = 5;
-    a.k =  3;
-    b.i = 9;
-    b.j = 4;
-    b.k = 0;
+    vector a(5,4,3),b(2,3,5);
     vector c = a + b; 
+    int dotproduct = a*b;
     std::cout<<c<<std::endl;
+    std::cout<<dotproduct<<std::endl;
 }
