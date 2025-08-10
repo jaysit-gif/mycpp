ded35c0aa28d6b04e11048f5d742aa72c4c10f2d
D J <jayadithya.dhurwey@gmail.com>
2025-08-10 12:53:23 +0530

    v0.2

--- rockpaperscissors.cpp
+++ rockpaperscissors.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <random>
+#include <string>
 using namespace std;
 
 void win(void){
@@ -16,12 +17,24 @@ char getQuitChoice() {
     }
     return q;
 }
-void gamelogic(void);
-
-/**how is v0.1 */
+void usermanual(void);
+void gamelogic(char a,char z);
+char input(void);
+char inputvalidator(char a);
+char computerplayer(void);
+/**how is v0.2 */
 int main(void){
+    char a;
+    char c;
+    usermanual();
     while(1){
-        gamelogic();
+        a = input();      
+        if(a == 't'){
+            cout<<"ERROR"<<endl;
+            break;
+        }
+        c = computerplayer();
+        gamelogic(a,c);
         if(getQuitChoice() == 'y'){
             break;
         }
@@ -30,14 +43,7 @@ int main(void){
     return 0;
 }
 
-void gamelogic(void){
-    char arr[3] = {'R','P','S'};
-    char a;
-    cout<<"YOUR TURN: ";
-    cin>>a;
-    mt19937 rng(std::random_device{}());
-    uniform_int_distribution<int> die(0,2);
-    char z = arr[die(rng)];
+void gamelogic(char a,char z){
     cout<<"computers turn: "<<z<<endl;
     if(a == 'P'&& z == 'R'){
         win();
@@ -50,4 +56,43 @@ void gamelogic(void){
     }else{
         cout<<"YOU LOSE"<<endl;
     }
+}
+
+char input(void){
+    char a;
+    cout<<"YOUR TURN: ";
+    cin>>a;
+    a = inputvalidator(a);
+    if(a == 'R'|| a == 'P' || a=='S'){
+        return a;
+    }else{
+        return 't';
+    }
+}
+
+void usermanual(void){
+    string z = "WELCOME\nFOR ROCK PRESS R\nFOR SCISSORS PRESS S\nFOR PAPER PRESS P\n";
+    cout<<z<<endl;
+}
+
+char inputvalidator(char a){
+    if(a == 'R'||a == 'S' || a == 'P'){
+        return a;
+    }else if(a == 'r'){
+        return 'R';
+    }else if(a == 's'){
+        return 'S';
+    }else if(a == 'p'){
+        return 'P';
+    }else{
+        return 't';
+    }
+}
+
+char computerplayer(void){
+    char arr[3] = {'R','P','S'};
+    mt19937 rng(std::random_device{}());
+    uniform_int_distribution<int> die(0,2);
+    char z = arr[die(rng)];
+    return z;
 }
